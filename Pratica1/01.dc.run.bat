f52394594d7a19dbd9ad10d1243a1079656c7a67
RASMiranda <34.ruimiranda@gmail.com>
2013-10-31 15:05:11 +0000

    corrigidos comentarios/mensagens

--- Pratica1/01.dc.run.bat
+++ Pratica1/01.dc.run.bat
@@ -4,7 +4,7 @@ REM Set SQL Server Instances Names from configuration file
 for /f "delims=" %%x in (00.config_inst.txt) do (set "%%x")
 
 
-REM Exercico 1 alinea b)----------------------------------------------------------------------------------------
+REM Exercico 1 alineas c) & d)----------------------------------------------------------------------------------------
 
 echo Running 01.c.0-CREATE-SEDE-ENCOMENDAS.sql
 sqlcmd -H localhost -S "%SEDE%" -i 01.c.0-CREATE-SEDE-ENCOMENDAS.sql
@@ -14,7 +14,7 @@ echo Running 01.d.0-CREATE-SEDE-SP-produtoEncomendadoFoiRecebido.sql
 sqlcmd -H localhost -S "%SEDE%" -i 01.d.0-CREATE-SEDE-SP-produtoEncomendadoFoiRecebido.sql
 if not %errorlevel% == 0 goto end
 
-echo Running 01.c.1-TEST.sql
+echo Running 01.dc.1-TEST.sql
 sqlcmd -H localhost -S "%SEDE%" -i 01.dc.1-TEST.sql
 if not %errorlevel% == 0 goto end
 
--- Pratica1/01.e.run.bat
+++ Pratica1/01.e.run.bat
@@ -4,7 +4,7 @@ REM Set SQL Server Instances Names from configuration file
 for /f "delims=" %%x in (00.config_inst.txt) do (set "%%x")
 
 
-REM Exercico 1 alinea b)----------------------------------------------------------------------------------------
+REM Exercico 1 alinea e)----------------------------------------------------------------------------------------
 
 
 echo Running 01.e.0-CREATE-SEDE-SP-produtoAlteraTipo.sql
@@ -12,7 +12,7 @@ sqlcmd -H localhost -S "%SEDE%" -i 01.e.0-CREATE-SEDE-SP-produtoAlteraTipo.sql
 if not %errorlevel% == 0 goto end
 
 
-echo Running 01.c.1-TEST.sql
+echo Running 01.e.1-TEST.sql
 sqlcmd -H localhost -S "%SEDE%" -i 01.e.1-TEST.sql
 if not %errorlevel% == 0 goto end
 
