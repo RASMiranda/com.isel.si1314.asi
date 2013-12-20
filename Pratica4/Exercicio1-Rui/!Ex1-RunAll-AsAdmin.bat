c9c05e25e1707eb423f55c9644b74f9825fbf461
RASMiranda <34.ruimiranda@gmail.com>
2013-12-20 16:31:35 +0000

    cont

--- Pratica4/Exercicio1-Rui/!Ex1-RunAll-AsAdmin.bat
+++ Pratica4/Exercicio1-Rui/!Ex1-RunAll-AsAdmin.bat
@@ -1,4 +1,3 @@
-REM Ex1.a
 @echo off
 
 setlocal enableextensions
@@ -7,23 +6,30 @@ cd /d "%~dp0"
 REM Set SQL Server Instances Names from configuration file
 for /f "delims=" %%x in (..\00.config_inst.txt) do (set "%%x")
 
+REM Ex1.a ========================================================
+
 call Ex1.a.Run.CREATE-DB-TBL.bat
 
-REM Ex1.b
+REM Ex1.b ========================================================
 
-REM TODO abrir numa nova janela
-REM call .\CódigoPrática4\Ex1\bin\Debug\Ex1.exe
+echo Running Ex1.exe
+start "" ".\Ex1Code\Ex1\bin\Debug\Ex1.exe"
 
-REM call ...Ex1Cliente.a...exe
+timeout 5
 
-REM Ex1.c.0
+echo Running Ex1Cliente.exe
+start "" ".\Ex1Code\Ex1Cliente.a\bin\Debug\Ex1Cliente.exe"
+
+REM Ex1.c.0 ======================================================
 
 REM call ...Ex1Cliente.c...exe
 
-REM Ex1.c.1
+REM Ex1.c.1 ======================================================
 
 REM kill ...Ex1...exe
 
 REM call ...Ex1.c...exe
 
 REM call ...Ex1Cliente.c...exe
+
+pause
