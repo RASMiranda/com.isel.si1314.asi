e4dff1f5dc9ac94703d2f0db7d5a2715bbdf02a2
David.MCCoelho <David.MCCoelho@gmail.com>
2014-02-07 19:38:53 +0000

    Refactoring dos scripts de bd para as Reclamacoes.

--- TrabalhoFinal/Implementacao/0.0.run.bat
+++ TrabalhoFinal/Implementacao/0.0.run.bat
@@ -7,21 +7,19 @@ REM Set SQL Server Instances Names from configuration file
 for /f "delims=" %%x in (.\0.0.config_setup.txt) do (set "%%x")
 
 
-
 echo Running 1.01.CreateTablesSede.sql
-sqlcmd -b -H localhost -S "%SEDE%" -i 1.01.CreateTablesSede.sql
+sqlcmd -b -H localhost -S "%SEDE%" -i 1.01.CreateTablesSede.sql -v ReclamacaoSeedValue=1
 if not %errorlevel% == 0 goto end
 
 
-echo Running 1.02.CreateTablesLoja-LSC.sql para lojas de produtos de crianca e senhora.
-sqlcmd -b -H localhost -S "%LSC%" -i 1.02.CreateTablesLoja-LSC.sql
-if not %errorlevel% == 0 goto end
-rem -v ClienteSeed=0
-
-echo Running 1.02.CreateTablesLoja-LHD.sql para lojas de produtos para homem e desportistas.
-sqlcmd -b -H localhost -S "%LHD%" -i 1.02.CreateTablesLoja-LHD.sql
+echo Running 1.02.CreateTablesLoja.sql para lojas de produtos de crianca e senhora.
+sqlcmd -b -H localhost -S "%LSC%" -i 1.02.CreateTablesLoja.sql -v ReclamacaoSeedValue=2
 if not %errorlevel% == 0 goto end
 rem -v ClienteSeed=0
+
+echo Running 1.02.CreateTablesLoja.sql para lojas de produtos para homem e desportistas.
+sqlcmd -b -H localhost -S "%LHD%" -i 1.02.CreateTablesLoja.sql -v ReclamacaoSeedValue=3
+if not %errorlevel% == 0 goto end
 
 
 echo Running 1.03.CreateSinonimosSede.sql na Sede.
@@ -29,28 +27,62 @@ sqlcmd -b -H localhost -S "%SEDE%" -i 1.03.CreateSinonimosSede.sql -v LSC="%LSC%
 if not %errorlevel% == 0 goto end
 
 
+
+echo Running 1.03.1.DropPublication-PeerToPeer-Reclamacao-Sede.sql na Sede.
+sqlcmd -b -H localhost -S "%SEDE%" -i 1.031.DropPublication-PeerToPeer-Reclamacao-Sede.sql -v SEDE="%SEDE%" LSC="%LSC%" LHD="%LHD%"
+if not %errorlevel% == 0 goto end
+
+echo Running 1.03.2.DropPublication-PeerToPeer-Reclamacao-Loja.sql na lojas de produtos de crianca e senhora.
+sqlcmd -b -H localhost -S "%LSC%" -i 1.032.DropPublication-PeerToPeer-Reclamacao-Loja.sql -v SEDE="%SEDE%" ESTA_LOJA="%LSC%" LOJA_REMOTA="%LHD%
+if not %errorlevel% == 0 goto end
+
+echo Running 1.03.2.DropPublication-PeerToPeer-Reclamacao-Loja.sql na loja de produtos para homem e desportistas.
+sqlcmd -b -H localhost -S "%LHD%" -i 1.032.DropPublication-PeerToPeer-Reclamacao-Loja.sql -v SEDE="%SEDE%" ESTA_LOJA="%LHD%" LOJA_REMOTA="%LSC%
+if not %errorlevel% == 0 goto end
+
+
+
+echo Running 1.03.5.CreatePublication-PeerToPeer-Reclamacao-Sede.sql na Sede.
+sqlcmd -b -H localhost -S "%SEDE%" -i 1.03.5.CreatePublication-PeerToPeer-Reclamacao-Sede.sql -v SEDE="%SEDE%" LSC="%LSC%" LHD="%LHD%"
+if not %errorlevel% == 0 goto end
+
+TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO
+goto end
+echo Running 1.03.6.CreatePublication-PeerToPeer-Reclamacao.sql na lojas de produtos de crianca e senhora.
+sqlcmd -b -H localhost -S "%LSC%" -i 1.03.6.CreatePublication-PeerToPeer-Reclamacao-Loja.sql -v SEDE="%SEDE%" ESTA_LOJA="%LSC%" LOJA_REMOTA="%LHD%
+if not %errorlevel% == 0 goto end
+
+echo Running 1.03.6.CreatePublication-PeerToPeer-Reclamacao.sql na loja de produtos para homem e desportistas.
+sqlcmd -b -H localhost -S "%LHD%" -i 1.03.6.CreatePublication-PeerToPeer-Reclamacao-Loja.sql -v SEDE="%SEDE%" ESTA_LOJA="%LHD%" LOJA_REMOTA="%LSC%
+if not %errorlevel% == 0 goto end
+
+
+
+
+
+
 echo Running 1.04.InserirDadosTeste na Sede.
 sqlcmd -b -H localhost -S "%SEDE%" -i 1.04.InserirDadosTeste.sql -v LSC="%LSC%" LHD="%LHD%"
 if not %errorlevel% == 0 goto end
 
 
-echo Running 0.25.sp_CrUD_Produto.sql na Sede.
-sqlcmd -b -H localhost -S "%SEDE%" -i 0.25.sp_CrUD_Produto.sql
+echo Running 1.25.sp_CrUD_Produto.sql na Sede.
+sqlcmd -b -H localhost -S "%SEDE%" -i 1.25.sp_CrUD_Produto.sql
 if not %errorlevel% == 0 goto end
 
-echo Running 0.30.sp_realizarEncomenda.sql na Sede.
-sqlcmd -b -H localhost -S "%SEDE%" -i 0.30.sp_realizarEncomenda.sql
+echo Running 1.30.sp_realizarEncomenda.sql na Sede.
+sqlcmd -b -H localhost -S "%SEDE%" -i 1.30.sp_realizarEncomenda.sql
 if not %errorlevel% == 0 goto end
 
-echo Running 0.35.sp_realizarVenda.sql na Sede.
-sqlcmd -b -H localhost -S "%SEDE%" -i 0.35.sp_realizarVenda.sql
+echo Running 1.35.sp_realizarVenda.sql na Sede.
+sqlcmd -b -H localhost -S "%SEDE%" -i 1.35.sp_realizarVenda.sql
 if not %errorlevel% == 0 goto end
-rem 0.35.testar sp_realizarVenda.sql
+rem 1.35.testar sp_realizarVenda.sql
 
-echo Running 0.40.sp_receberEncomenda.sql na Sede.
-sqlcmd -b -H localhost -S "%SEDE%" -i 0.40.sp_receberEncomenda.sql
+echo Running 1.40.sp_receberEncomenda.sql na Sede.
+sqlcmd -b -H localhost -S "%SEDE%" -i 1.40.sp_receberEncomenda.sql
 if not %errorlevel% == 0 goto end
-rem 0.40.testar sp_receberEncomenda.sql
+rem 1.40.testar sp_receberEncomenda.sql
 
 
 :end
